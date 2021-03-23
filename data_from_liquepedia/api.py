2b1aba7b96dd8c525a735bf12ed01bf119a8c066
cabutchei <63674956+cabutchei@users.noreply.github.com>
2021-03-22 23:05:12 -0300

    Add files via upload

---
+++ data_from_liquepedia/api.py
@@ -0,0 +1,40 @@
+import pandas, requests
+from bs4 import BeautifulSoup
+
+def game_url(game):
+    return "https://liquipedia.net/{}/Portal:Statistics".format(game)
+
+game = input()
+
+url = game_url(game)
+
+page = requests.get(url)
+
+if page.status_code == 404:
+    raise Exception("Invalid URL")
+
+html_text = page.content
+
+
+soup = BeautifulSoup(html_text, "html.parser")
+span = soup.find(id = "By_player")
+
+h3 = span.parent
+
+div = h3.next_sibling.next_sibling.find("tbody")
+
+table = div.find_all("tr")[1:]
+
+table_names = []
+
+number_of_players = len(table)
+
+for i in range(number_of_players):
+    table_names.append([table[i].find_all("a")[1].text])
+
+data = pandas.DataFrame(table_names , columns = [["Melhores Jogadores"]])
+
+#print(data)
+
+#data.to_csv("data.csv")
+
