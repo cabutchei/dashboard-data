0f675518e8f1e98005aa724c2a17f74e0dfac390
cabutchei <63674956+cabutchei@users.noreply.github.com>
2021-04-18 17:00:44 -0300

    Update top_games_twitch_plot.py

--- twitch_data/top_games_twitch_plot.py
+++ twitch_data/top_games_twitch_plot.py
@@ -4,6 +4,9 @@ import pandas
 data = pandas.read_csv("C:/Users/Usuario/Documents/Visual Studio Code/dashboard/twitch_data/top_games_twitch_stats.csv") 
 games = list(data["Nome"])[:8] #lista com os nomes
 games.reverse() #o reverse se faz necessário para que o plotly disponha os primeiros colocados na parte de cima
+average_views = list(data["Visualização Média"])[:8] #<--atualizado
+average_views.reverse() #<--atualizado
+average_views = list(map(lambda view : "Visualização Média: " + str(view), average_views)) #<--atualizado
 percentages = list(map(lambda percentage : float(percentage[:-1]), data["Frequência no Top 3"]))[:8] #fazer lista com as porcentagens,
 percentages.reverse()                                                                           #eliminando o "%" e convertendo em float
 
@@ -12,6 +15,7 @@ fig = go.Figure()
 fig.add_trace(go.Bar(
     y= games, #os nomes comporão a ordenada e as porcentagens, a abscissa
     x=percentages,
+    text = average_views, #<--atualizado
     orientation='h', #vertical ou horizontal
     marker=dict(
         color='#9803fc', #cor da barra
