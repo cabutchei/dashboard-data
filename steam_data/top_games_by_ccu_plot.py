26fd1b901fdc4e03d607f34b69cfd964f0156b78
cabutchei <63674956+cabutchei@users.noreply.github.com>
2021-04-19 00:35:06 -0300

    Add files via upload

---
+++ steam_data/top_games_by_ccu_plot.py
@@ -0,0 +1,54 @@
+import plotly.graph_objects as go
+import pandas
+
+data = pandas.read_csv("C:/Users/Usuario/Documents/Visual Studio Code/dashboard/steam_data/top_games_by_ccu_stats.csv") 
+games = list(data["Nome"])[:8] #lista com os nomes
+games.reverse() #o reverse se faz necessário para que o plotly disponha os primeiros colocados na parte de cima
+average_ccu = list(data["Média de ccu"])[:8]
+average_ccu.reverse()
+
+
+
+fig = go.Figure()
+
+fig.add_trace(go.Bar(
+    y= games, #os nomes comporão a ordenada e as porcentagens, a abscissa
+    x=average_ccu,
+    orientation='h', #vertical ou horizontal
+    marker=dict(
+        color='#1b0261', #cor da barra
+        line=dict(color='#1b0261', width=3) #cor da borda da barra
+    )
+))
+
+fig.update_layout(paper_bgcolor = 'white',
+                plot_bgcolor = 'white')
+
+
+def layout(name, ccu): #layout seta um dado jogo no gráfico junto com sua boxart de acordo com os parâmetros informados
+    formatted = name.replace(" ", "%20")
+    if name == "Source SDK Base 2013 Multiplayer":
+        source = "https://res-3.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco/b17gr3xreillrivarc3d"
+    else:
+        source = "https://static-cdn.jtvnw.net/ttv-boxart/{}-100x100.jpg".format(formatted)
+    fig.add_layout_image(
+        dict(
+            source = source,
+            xref="x",
+            yref="y",
+            x = ccu,
+            y = name,
+            yanchor = "middle",
+            xanchor = "left",
+            sizex = 50000,
+            sizey = 0.85
+        )
+)
+
+
+for i in range(8):
+    layout(games[i], average_ccu[i])
+
+
+
+fig.show()
---
+++ steam_data/top_games_by_ccu_stats.py
@@ -0,0 +1,35 @@
+import pandas
+data = pandas.read_csv("C:/Users/Usuario/Documents/Visual Studio Code/dashboard/steam_data/top_games_by_ccu - Copy.csv")
+number_of_entries = len(data.Nome)
+number_of_groups = number_of_entries // 8
+all_entries = list(data.Nome)
+all_games = []
+
+def find_all(parameter, iterable): #retorna uma lista com os índices de todas as instâncias de parameter dentro de um iterável
+    indexes = []
+    i = 0
+    for element in iterable:
+        if element == parameter:
+            indexes.append(i)
+        i += 1
+    return indexes
+
+for game in data.Nome: #preenche all_games com todos os jogos de maneira única
+    if all_games.count(game) == 0:
+        all_games.append(game)
+
+
+rows = []
+for game in all_games: 
+    total_ccu = 0
+    indexes = find_all(game, data.Nome)
+    for index in indexes: #iterando sobre a lista retornada por find_all
+        total_ccu += data.ccu[index] #atualizando a conta de total_ccu para cada ocorrência(índice)
+    occurrences = len(indexes)
+    average_ccu = int(round(total_ccu / occurrences))
+    rows.append([game, average_ccu])
+rows.sort(reverse = True, key = lambda x : x[1])    
+df = pandas.DataFrame(rows, columns = ["Nome", "Média de ccu"])
+#print(df)
+#df.to_csv("C:/Users/Usuario/Documents/Visual Studio Code/dashboard/steam_data/top_games_by_ccu_stats.csv", index = False, encoding = "utf-8-sig")
+
