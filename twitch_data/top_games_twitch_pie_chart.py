977afddb464752610a2305fa149ea4bdd3cf1e05
cabutchei <63674956+cabutchei@users.noreply.github.com>
2021-04-20 16:14:43 -0300

    Update top_games_twitch_pie_chart.py

--- twitch_data/top_games_twitch_pie_chart.py
+++ twitch_data/top_games_twitch_pie_chart.py
@@ -2,7 +2,7 @@ import pandas
 import plotly.express as px
 
 df = pandas.read_csv("C:/Users/Usuario/Documents/Visual Studio Code/dashboard/twitch_data/top_games_twitch_stats.csv").filter(items = ["Nome", "Top 1"])
-df = df[lambda x : x["Top 1"] > 0]
+df = df[lambda x : x["Top 1"] > 0] #pegando só os jogos que apareceram pelo menos uma vez no top 1
 fig = px.pie(df, values = "Top 1", names = "Nome")
 fig.update_layout(title = "Top jogos da Twitch por frequência no primeiro lugar")
 fig.show()
