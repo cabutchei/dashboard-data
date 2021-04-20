f1a29c50a1ae86cdc5f095b340ce58a6997eea08
cabutchei <63674956+cabutchei@users.noreply.github.com>
2021-04-20 16:13:56 -0300

    Add files via upload

---
+++ twitch_data/top_games_twitch_pie_chart.py
@@ -0,0 +1,8 @@
+import pandas
+import plotly.express as px
+
+df = pandas.read_csv("C:/Users/Usuario/Documents/Visual Studio Code/dashboard/twitch_data/top_games_twitch_stats.csv").filter(items = ["Nome", "Top 1"])
+df = df[lambda x : x["Top 1"] > 0]
+fig = px.pie(df, values = "Top 1", names = "Nome")
+fig.update_layout(title = "Top jogos da Twitch por frequência no primeiro lugar")
+fig.show()
